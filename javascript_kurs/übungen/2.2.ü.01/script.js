c2ef300c4dec6e3dc1b0921f3c851fb4cf3f8677
Stefan Brandt <stefan_brandt@cdemy.de>
2025-12-01 11:39:34 +0100

    first commit

---
+++ javascript_kurs/übungen/2.2.ü.01/script.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    main();
+}
+
+function main() {
+
+    let benutzerName = "Stefan"
+    let alter = 32
+    console.log(`Hallo, mein Name ist ${benutzerName} und ich bin ${alter} Jahre alt.`);
+}
